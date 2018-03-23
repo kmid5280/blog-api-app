e1d505d6874e86addbe40033c272b7946e01faca
Kyle Middleton <kmiddleton654@gmail.com>
2018-03-23 16:24:17 -0600

    second commit

--- addTests.js
+++ addTests.js
@@ -80,7 +80,92 @@ describe('GET endpoint', function() {
             .then(function(count) {
                 expect(res.body.blogposts).to.have.length.of(count);
             })
+            
     })
 
-    //add code here
+    it('should return blog posts with right fields', function() {
+
+        let resBlogPost;
+        return chai.request(app)
+            .get('/blogposts')
+            .then(function(res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body.blogposts).to.be.a('array');
+                expect(res.body.blogposts).to.have.length.of.at.least(1);
+                res.body.blogpsts.forEach(function(post) {
+                    expect(post).to.be.a('object');
+                    expect(post).to.include.keys(
+                        'title', 'content', 'author');
+                })
+                resBlogPost = res.body.blogposts[0];
+                return BlogPost.findById(resBlogPost.id);
+            })
+            .then(function(post) {
+                expect(resBlogPost.id).to.equal(post.id);
+                expect(resBlogPost.title).to.equal(post.title);
+                expect(resBlogPost.content).to.equal(post.content);
+                expect(resBlogPost.author).to.equal(post.author);
+            })
+    } )
+})
+
+describe('POST endpoint', function () {
+    it('should add a new blog post', function() {
+        const newBlogPost = generateBlogPostData();
+        
+        return chai.request(app)
+            .post('/blogposts')
+            .send(newBlogPost)
+            .then(function(res) {
+                expect(res).to.have.status(201);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.include.keys(
+                    'title', 'content', 'author');
+                expect(res.body.title).to.equal(newBlogPost.title);
+                expect(res.body.content).to.equal(newBlogPost.content)
+                expect(res.body.author).to.equal(newBlogPost.author)
+                return BlogPost.findById(res.body.id)
+            })
+            .then(function(post) {
+                expect(post.title).to.equal(newBlogPost.title);
+                expect(post.content).to.equal(newBlogPost.content);
+                expect(post.author).to.equal(newBlogPost.author)
+            })
+    })
+})
+
+describe('PUT endpoint' function() {
+    it('should update fields you send over', function() {
+        const updateData = {
+            title: 'new updated title',
+            content: 'newly updated content'
+        }
+
+        return Restaurant
+            .findOne()
+            .then(function(post) {
+                updateData.id = post.id;
+                return chai.request(app)
+                    .put(`/blogposts/${post.id}`)
+                    .send(updateData);
+            })
+            .then(function(res) {
+                expect(res).to.have.status(204);
+                return BlogPost.findById(updateData.id)
+            })
+            .then(function(post) {
+                expect(post.title).to.equal(updateData.title);
+                expect(post.content).to.equal(updateData.content);
+                expect(post.author).to.equal(post.author)
+            })
+    })
+})
+
+describe('DELETE endpoint', function() {
+    it('should delete a post by id', function() {
+        let post;
+        return BlogPost
+    })
 })
